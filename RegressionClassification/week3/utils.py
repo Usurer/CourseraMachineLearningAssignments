7604b018b31ab138d11921de8ae0d44a754bed67
Usurer <ivvlg@narod.ru>
2023-05-03 10:52:33 +0000

    Non-linear logistic regression with second dataset

--- RegressionClassification/week3/utils.py
+++ RegressionClassification/week3/utils.py
@@ -12,5 +12,8 @@ def plot_data(X, y, pos_label="y=1", neg_label="y=0"):
     negative = y == 0
     
     # Plot examples
+    # I don't understand how indexing access works in this case, since positive and negative are bool arrays on their own, 
+    # but it seems that it serves as a filter, i.e. X[negative] returns only those values which index corresponds to an index with y == 0
     plt.plot(X[positive, 0], X[positive, 1], 'k+', label=pos_label)
     plt.plot(X[negative, 0], X[negative, 1], 'yo', label=neg_label)
+    
